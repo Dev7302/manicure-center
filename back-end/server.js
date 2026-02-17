885810d3f6916fba120d64360bfbc77f9884d1d2
Dev7302 <seuemail@example.comenzo.frangiotte@gmail.com>
2026-02-17 15:38:01 -0300

    remove fixed port 8080

--- back-end/server.js
+++ back-end/server.js
@@ -26,11 +26,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/agendamentos", agendamentoRoutes);
 
 // Use a porta fornecida pelo Railway (importante!)
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-});
-app.get('/', (req, res) => {
-  res.send('Servidor online');
 });
