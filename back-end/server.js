cab304590ab72b51317108ea9e4fad9df3f36efa
Dev7302 <seuemail@example.comenzo.frangiotte@gmail.com>
2026-02-17 15:35:00 -0300

    fix railway port

--- back-end/server.js
+++ back-end/server.js
@@ -26,7 +26,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/agendamentos", agendamentoRoutes);
 
 // Use a porta fornecida pelo Railway (importante!)
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
